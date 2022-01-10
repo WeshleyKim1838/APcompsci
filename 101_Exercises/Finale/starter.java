8469898a02fa940d335287c09ae57cb8770a9022
EC2 Default User <ec2-user@ip-172-31-20-229.ec2.internal>
2022-01-10 19:31:50 +0000

    first push

---
+++ 101_Exercises/Finale/starter.java
@@ -0,0 +1,17 @@
+import pkg.*;
+import java.util.Scanner;
+import java.util.Random;
+
+
+class starter {
+	public static void main(String args[]) {
+		// Your code goes below here
+		public class Hangman{
+			private String guessingWord;
+			private int numTries;
+			Hangman
+		}
+
+		
+	}
+}
